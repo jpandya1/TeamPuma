5d2899b1e3c6dfa1b7582d0d3ce3dd1d541de0f9
Penichet <penichet@utexas.edu>
2019-10-25 18:38:00 -0500

    more tests

--- website/api/test.js
+++ website/api/test.js
@@ -1,28 +1,35 @@
 const operations = require('./operations.js') 
 const assert = require('assert')
-const searchpage = require('.')
+const searchpage = require('./searchpage.html')
 
+//test 1
 it('Correctly accepts keys', () => {
   var myTextBox = searchpage.findElement(webdriver.By.id('searchField'));
   myTextBox.sendKeys('aluminum');
-
-  //You will probably want to use getAttribute()
   var elementContent = myTextBox.getText();
   assert.equal(elementContent, 'aluminum');
 })
-
-it('Correctly accepts keys', () => {
-
-
+//test 2
+it('Correctly doesnt show anything for empty', () => {
+	var myTextBox = searchpage.findElement(webdriver.By.id('searchField'));
+  	var elementContent = myTextBox.getText();
+  	assertTrue(elementContent.size() === 0);
+	})
+//test 3
+it('Reset button clears entry', () => {
+	searchpage.Reset();
+	var myTextBox = searchpage.findElement(webdriver.By.id('searchField'));
+	var elementContent = myTextBox.getText();
+  	assertTrue(elementContent.size() === 0);
 	})
 
-it('Correctly accepts keys', () => {
-
-
-	})
-
-it('Correctly accepts keys', () => {
-
+it('Multiple resets with text inbetween is still empty', () => {
+	searchpage.Reset();
+	var myTextBox = searchpage.findElement(webdriver.By.id('searchField'));
+    myTextBox.sendKeys('aluminum');
+    searchpage.Reset();
+    var elementContent = myTextBox.getText();
+  	assertTrue(elementContent.size() === 0);
 
 	})
 
