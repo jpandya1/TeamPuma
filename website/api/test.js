e4d32c45533955b62ede3946843e79dda59c8ecd
Penichet <penichet@utexas.edu>
2019-10-25 18:28:53 -0500

    first test

---
+++ website/api/test.js
@@ -0,0 +1,138 @@
+const operations = require('./operations.js') 
+const assert = require('assert')
+const searchpage = require('.')
+
+it('Correctly accepts keys', () => {
+  var myTextBox = searchpage.findElement(webdriver.By.id('searchField'));
+  myTextBox.sendKeys('aluminum');
+
+  //You will probably want to use getAttribute()
+  var elementContent = myTextBox.getText();
+  assert.equal(elementContent, 'aluminum');
+})
+
+it('Correctly accepts keys', () => {
+
+
+	})
+
+it('Correctly accepts keys', () => {
+
+
+	})
+
+it('Correctly accepts keys', () => {
+
+
+	})
+
+it('Correctly accepts keys', () => {
+
+
+	})
+
+it('Correctly accepts keys', () => {
+
+
+	})
+
+it('Correctly accepts keys', () => {
+
+
+	})
+
+it('Correctly accepts keys', () => {
+
+
+	})
+
+it('Correctly accepts keys', () => {
+
+
+	})
+
+it('Correctly accepts keys', () => {
+
+
+	})
+
+it('Correctly accepts keys', () => {
+
+
+	})
+
+it('Correctly accepts keys', () => {
+
+
+	})
+
+it('Correctly accepts keys', () => {
+
+
+	})
+
+it('Correctly accepts keys', () => {
+
+
+	})
+
+it('Correctly accepts keys', () => {
+
+
+	})
+
+it('Correctly accepts keys', () => {
+
+
+	})
+
+it('Correctly accepts keys', () => {
+
+
+	})
+
+it('Correctly accepts keys', () => {
+
+
+	})
+
+it('Correctly accepts keys', () => {
+
+
+	})
+
+it('Correctly accepts keys', () => {
+
+
+	})
+
+it('Correctly accepts keys', () => {
+
+
+	})
+
+it('Correctly accepts keys', () => {
+
+
+	})
+
+it('Correctly accepts keys', () => {
+
+
+	})
+
+it('Correctly accepts keys', () => {
+
+
+	})
+
+it('Correctly accepts keys', () => {
+
+
+	})
+
+it('Correctly accepts keys', () => {
+
+
+	})
+
