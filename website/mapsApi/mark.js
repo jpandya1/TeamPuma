a9c4abd46ad726db0d80080ad15cb49eee542dbb
Kan <kanvan@utexas.edu>
2019-11-15 19:44:35 -0600

    maps merge

--- website/mapsApi/mark.js
+++ website/mapsApi/mark.js
@@ -117,13 +117,9 @@ function clearSearchList() {
     document.getElementById('APIResponseList').innerHTML = "";
 }
 
-<<<<<<< HEAD
+
 function geocode() {
     var location = document.getElementById('searchFieldLocation').value;
-=======
-geocode = function() {
-    var location = document.getElementById('searchField').value;
->>>>>>> b49951ebd4d593a8b8236ed628cf372d3bb529de
     axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
             params: {
                 address: location,
@@ -182,12 +178,5 @@ function addMarker(prop) {
     });
 }
 
-<<<<<<< HEAD
-
-=======
-getList = function(){
-  return document.getElementById('APIResponseList');
-}
->>>>>>> b49951ebd4d593a8b8236ed628cf372d3bb529de
 
 module.exports = convert;
